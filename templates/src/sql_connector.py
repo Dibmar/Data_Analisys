a99fcaa93fd6c4269dd61452e97ed57ac712d5ad
unknown <diomedes.barbero@gmail.com>
2021-03-10 17:33:39 +0100

    10/03/21 SQL_General class added. It acts as the father to both MySQL_Manager and Postgre_SQL_Manager. The latter one is a Postgre SQL interface with python

--- templates/src/sql_connector.py
+++ templates/src/sql_connector.py
@@ -8,23 +8,13 @@ import sys
 # For data export as csv and/or dataframe
 import pandas as pd
 
-
-class MySQL_manager(object):
-    """
-    This class has been created to establish a connection with a MySQL server database. For this it uses:
-        
-        - The json library
-        - The pymysql library
-        - The slalchemy/create_engine library
-        - The pandas library
-
-    This class can make queries in MySQL and return the results as a dataframe for further exploration.
-    """
+class General_SQL (object):
     def __init__(self, path):
         self.path = path
 
 
-    def json_to_dict(self):
+    # For reading config files
+    def read_config(self):
         """
                             ---What it does---
         Loads a json file and turns it into a dictionary.
@@ -37,9 +27,9 @@ class MySQL_manager(object):
         """
         try:
             with open(self.path, 'r+') as outfile:
-                settings = json.load(outfile)
-                
-                return settings
+                self.settings = json.load(outfile)
+                              
+                return self.settings
             
         except Exception as error:
             raise ValueError(error)
@@ -61,31 +51,210 @@ class MySQL_manager(object):
         This function returns a MySQL database ready for connection
         """
 
-        try:
-            with open(self.path, 'r+') as outfile:
-                settings = json.load(outfile)
-                
+        try:             
+            self.settings = self.settings['MySQL']
 
-            self.IP_DNS = settings["IP_DNS"]
-            self.USER = settings["USER"]
-            self.PASSWORD = settings["PASSWORD"]
-            self.DATABASE = settings["BD_NAME"]
-            self.PORT = settings["PORT"]
+            IP_DNS = self.settings["IP_DNS"]
+            USER = self.settings["USER"]
+            PASSWORD = self.settings["PASSWORD"]
+            DATABASE = self.settings["DB_NAME"]
+            PORT = self.settings["PORT"]
 
-            self.connection = pymysql.connect(host=self.IP_DNS, user=self.USER, 
-                                    password=self.PASSWORD, database=self.DATABASE, 
-                                    port=self.PORT)
+            self.connection = pymysql.connect(host= IP_DNS, user= USER, 
+                                    password= PASSWORD, database= DATABASE, 
+                                    port= PORT)
             
             self.cursor = self.connection.cursor()
-            
-            # self.engine = create_engine(f'mysql+pymysql://user:{self.USER}/DB?local_infile=1') 
-            
-            print(f"Connected to MySQL server {self.DATABASE}")
+                     
+            print(f"Connected to MySQL server {DATABASE}")
                                             
             return self.connection, self.cursor
        
         except Exception as error:
             raise ValueError(error)        
+    
+    
+    def CREATE_a_postgre_db (self):
+        """
+                            ---What it does---
+        This function creates a postgre SQL database connection with Python.
+
+                            ---What it needs---
+            - This function needs access to a json with the connection settings (self.path)
+
+                            ---What it returns---
+        This function returns the cursor object.
+        """
+                
+        try:
+            self.settings = self.settings['Postgre_SQL']
+
+            localhost = self.settings['HOST']
+            user = self.settings['USER']
+            password = self.settings['PASSWORD']
+            dbname = self.settings['DB_NAME']
+            self.schema = self.settings['SCHEMA']
+
+            try:
+                self.connection = psycopg2.connect(f"host={localhost} dbname= {dbname} user= {user} password= {password}")   
+                self.cursor =  self.connection.cursor()
+
+                print(f'Succesfull connection to {dbname}, in schema {self.schema}')
+
+            except psycopg2.DatabaseError:
+                if self.connection:
+                    self.connection.rollback()
+                
+                print ('Error %s' % psycopg2.DatabaseError)  
+                sys.exit(1)
+                        
+        except Exception as error:
+            raise ValueError(error)
+
+
+    # General queries
+    def ORDER_custom_query(self, query, wish= False):
+        """
+                            ---What it does---
+        This function executes a custom query. Then prints it.
+
+                            ---What it needs---
+            - A query in string format and in the proper sintax
+            - A boolean value (wish) if you wish to return the query in dataframe format.
+
+                            ---What it returns---
+        This function does not return anything
+        """
+        
+        print(query)
+        table =  pd.read_sql(query, con= self.connection)
+        print(table)
+
+        if wish == True:
+            print('Returning your query')
+
+            return table
+
+
+    def ORDER_select_all_from (self, table):
+        """
+                            ---What it does---
+        This function prints all available data in dataframe format.
+        
+                            ---What it needs---
+            - The name of the (table)
+            
+                            ---What it returns---
+        This function does not return anything
+        """
+
+        query = f'SELECT * FROM {table}'
+        
+        table =  pd.read_sql(query, con= self.connection)
+
+        print(table)
+
+
+    def ORDER_create_a_df_from_sql(self, query= None, return_file= True, store= False, name= 'my_file', destination='Output/'):
+        """
+                            ---What it does---
+        Creates a dataframe from a given sql table and returns it and/or saves it locally.
+        This table must be generated through a MySQL query.
+
+                            ---What it needs---
+            - A valid SQL query (query). It should be in string format.
+            - A boolean value
+            - A boolean value (store) if you wish to save the dataframe in csv format locally. Set to False by default.
+            - The name of the file to save (name) it should be in string format. Set to 'my_file' by default.
+            - The directory to be saved in (destination). It should be in string format end with '/'. Set as 'Output/' by default.
+        
+                            ---What it returns---
+        This function returns a dataframe object
+        """
+        self.df = pd.read_sql(query, con= self.connection)
+
+        if return_file == True and store == False:
+    
+            return self.df
+        
+        elif return_file == True and store == True:
+            self.name = f'{name}.csv'
+
+            if destination.endswith('/'):
+                self.stored_in = f'{destination}{self.name}'
+            
+            else:
+                self.stored_in = f'{destination}/{self.name}'
+
+            print(f'The file will be stored as "{name}"  in {destination}...')
+            self.df.to_csv(self.stored_in)
+
+            return self.df
+
+
+    def ORDER_join_tables (self, table, table_2, on, selection= '*', how= 'inner'):
+        """
+                            ---What it does---
+        Joins the selected tables in your database on the column of your choice, selection and method.
+
+                            ---What it needs---
+            - Names of your tables (table, table_2) in  string format.
+            - The column to join on (on) in string format.
+            - Your selection (selection), by default set to '*'.
+            - The method of join (how), by default set to 'inner'.
+        """
+        if how == 'left' or how == 'LEFT':
+            self.command = f"""SELECT {selection} 
+                FROM {table} 
+                LEFT JOIN {table_2} ON {on};"""
+        
+        elif how == 'right' or how == 'RIGHT':
+            self.command = f"""SELECT {selection} 
+                FROM {table} 
+                RIGHT JOIN {table_2} ON {on};"""
+
+        elif how == 'inner' or how == 'INNER':
+            self.command = f"""SELECT {selection} 
+                FROM {table} 
+                INNER JOIN {table_2} ON {on};"""
+
+        elif how == 'full' or how == 'FULL':
+            self.command = f"""SELECT {selection} FROM {table}
+                    LEFT JOIN {table_2} ON {on}
+                    UNION
+                    SELECT {selection} FROM {table_2}
+                    LEFT JOIN {table} ON {on}"""
+
+        self.cursor.execute(f"{self.command}")
+        
+
+    # For closing your db
+    def ORDER_close(self):
+        """
+                                ---What it does---
+        Closes conection with your database
+        """
+
+        print(f"Closing connection connection with MySQL server...")
+        self.cursor.close()
+        self.connection.close()
+
+
+class MySQL_manager(General_SQL):
+    """
+    This class has been created to establish a connection with a MySQL server database. For this it uses:
+        
+        - The json library
+        - The pymysql library
+        - The slalchemy/create_engine library
+        - The pandas library
+
+    This class can make queries in MySQL and return the results as a dataframe for further exploration.
+    """
+    def __init__(self, path):
+        self.path = path
+
+
 
 
     # Quick oders for your database
@@ -146,42 +315,6 @@ class MySQL_manager(object):
         self.cursor.execute(f"DROP TABLE IF EXISTS {table}")
 
 
-    def ORDER_join_tables (self, table, table_2, on, selection= '*', how= 'inner'):
-        """
-                            ---What it does---
-        Joins the selected tables in your database on the column of your choice, selection and method.
-
-                            ---What it needs---
-            - Names of your tables (table, table_2) in  string format.
-            - The column to join on (on) in string format.
-            - Your selection (selection), by default set to '*'.
-            - The method of join (how), by default set to 'inner'.
-        """
-        if how == 'left' or how == 'LEFT':
-            self.command = f"""SELECT {selection} 
-                FROM {table} 
-                LEFT JOIN {table_2} ON {on};"""
-        
-        elif how == 'right' or how == 'RIGHT':
-            self.command = f"""SELECT {selection} 
-                FROM {table} 
-                RIGHT JOIN {table_2} ON {on};"""
-
-        elif how == 'inner' or how == 'INNER':
-            self.command = f"""SELECT {selection} 
-                FROM {table} 
-                INNER JOIN {table_2} ON {on};"""
-
-        elif how == 'full' or how == 'FULL':
-            self.command = f"""SELECT {selection} FROM {table}
-                    LEFT JOIN {table_2} ON {on}
-                    UNION
-                    SELECT {selection} FROM {table_2}
-                    LEFT JOIN {table} ON {on}"""
-
-        self.cursor.execute(f"{self.command}")
-
-
     def ORDER_insert_into_table(self, table, columns, values):
         """
                             ---What it does---
@@ -202,77 +335,6 @@ class MySQL_manager(object):
         query = f"INSERT INTO {table} {columns} VALUES {values};"
         print(query)
 
-        self.cursor.execute(query)
-
-
-    def ORDER_create_a_df_from_sql(self, query= None, return_file= True, store= False, name= 'my_file', destination='Output/'):
-        """
-                            ---What it does---
-        Creates a dataframe from a given sql table and returns it and/or saves it locally.
-        This table must be generated through a MySQL query.
-
-                            ---What it needs---
-            - A valid MySQL query (query). It should be in string format.
-            - A boolean value
-            - A boolean value (store) if you wish to save the dataframe in csv format locally. Set to False by default.
-            - The name of the file to save (name) it should be in string format. Set to 'my_file' by default.
-            - The directory to be saved in (destination). It should be in string format end with '/'. Set as 'Output/' by default.
-        
-                            ---What it returns---
-        This function returns a dataframe object
-        """
-        self.df = pd.read_sql(query, con= self.connection)
-
-        if return_file == True and store == False:
-    
-            return self.df
-        
-        elif return_file == True and store == True:
-            self.name = f'{name}.csv'
-
-            if destination.endswith('/'):
-                self.stored_in = f'{destination}{self.name}'
-            
-            else:
-                self.stored_in = f'{destination}/{self.name}'
-
-            print(f'The file will be stored as "{name}"  in {destination}...')
-            self.df.to_csv(self.stored_in)
-
-            return self.df
-
-
-    def ORDER_select_all_from (self, table):
-        """
-                            ---What it does---
-        This function prints all available data in dataframe format.
-        
-                            ---What it needs---
-            - The name of the (table)
-            
-                            ---What it returns---
-        This function does not return anything
-        """
-
-        query = f'SELECT * FROM {table}'
-        
-        table =  pd.read_sql(query, con= self.connection)
-
-        print(table)
-
-
-    def ORDER_custom_query(self, query):
-        """
-                            ---What it does---
-        This function executes a custom query. Then prints it.
-
-                            ---What it needs---
-            - A query in string format and in the proper sintax
-
-                            ---What it returns---
-        This function does not return anything
-        """
-        print(query)
         self.cursor.execute(query)
 
 
@@ -311,18 +373,8 @@ class MySQL_manager(object):
             print('Quitting')
 
 
-    def ORDER_close(self):
-        """
-                                ---What it does---
-        Closes conection with your database
-        """
 
-        print(f"Closing connection connection with MySQL server...")
-        self.cursor.close()
-        self.connection.close()
-
-
-class Postgre_SQL_manager(MySQL_manager):
+class PostgreSQL_manager(General_SQL):
     """
     This class has been created to establish a connection with a Postgre SQL database. For this it uses:
         
@@ -339,41 +391,28 @@ class Postgre_SQL_manager(MySQL_manager):
         self.path = path
 
 
-    def CREATE_a_postgre_db (self):
+
+
+
+    def ORDER_show_tables (self):
+        query = f"""
+        SELECT *
+        FROM pg_catalog.pg_tables
+        WHERE schemaname = '{self.schema}'
         """
-                            ---What it does---
-        This function creates a postgre SQL database connection with Python.
+        print(query)
+        table =  pd.read_sql(query, con= self.connection)
+        print(table)
 
-                            ---What it needs---
-            - This function needs access to a json with the connection settings (self.path)
 
-                            ---What it returns---
-        This function returns the cursor object.
+
+    def ORDER_close(self):
         """
-                
-        try:
-            with open(self.path, 'r+') as outfile:
-                settings = json.load(outfile) 
-                    
-            localhost = settings['localhost']
-            user = settings['user']
-            password = settings['password']
-            dbname = settings['dbname']
+                                ---What it does---
+        Closes conection with your database
+        """
 
-            try:
-                self.conection = psycopg2.connect(f"host={localhost} dbname= {dbname} user= {user} password= {password}")   
-                self.cursor =  self.conection.cursor()
-
-                return self.cursor
-
-            except psycopg2.DatabaseError:
-                if self.conection:
-                    self.conection.rollback()
-                
-                print ('Error %s' % psycopg2.DatabaseError)  
-                sys.exit(1)
-                        
-        except Exception as error:
-            raise ValueError(error)
-
-
+        print(f"Closing connection connection with MySQL server...")
+        self.connection.close()
+        self.cursor.close()
+        
