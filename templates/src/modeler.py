45b2d0445675f7aebfd4166b33f9200e637954e0
Dibmar <diomedes.barbero@gmail.com>
2021-03-26 18:26:01 +0100

    26/03/21 Dibmar. Documentation added for functions located in modeler.py. Also in modeler, new function added: scale_data. This function is capable of making either a Standard or MinMax scaler

--- templates/src/modeler.py
+++ templates/src/modeler.py
@@ -39,59 +39,110 @@ from xgboost import XGBClassifier
 class Model_Maker():
     def __init__(self, dataframe= None):
         self.df = dataframe
-        self.label_enc = LabelEncoder()
 
 
     def column_encoder(self, column= None, position= None):
         """
                             ---What it does---
-        This function
+        This function encodes one column of your dataframe using Label Encoder and returns it.
 
                             ---What it needs---
-            - 
+            - You to choose between
+                * The name of your column (column). It should be in string format
+                * The position of your column (position). It should be int
+            For the encoding to take place.
 
                             ---What it return---
-        This function
+        This function returns your encoded column
         """
+        label_enc = LabelEncoder()
         
         if column != None:
-            self.column_encoded = self.label_enc.fit_transform(self.df[column])
+            self.column_encoded = label_enc.fit_transform(self.df[column])
         
         elif position != None:
-            self.column_encoded = self.label_enc.fit_transform(self.df.iloc[:, position])
+            self.column_encoded = label_enc.fit_transform(self.df.iloc[:, position])
         
         return self.column_encoded
     
 
-    def data_splitter(self, train_list= None, test= None, test_size= 0.33, random_state= 42, scale_data= True):
+    def data_splitter(self, train_list= None, name_test= None, range_of_columns= None, 
+                    index_test= None, test_size= 0.33, random_state= 42, 
+                    shuffle= True, stratify= None):
         """
                             ---What it does---
-        This function
+        This function splits your data into a Train and Test baches, then returns them.
 
                             ---What it needs---
-            - 
+            - You to choose between
+                * The list of columns for train (train_list) and name of the colum for test (name_test).
+                    It should be a list containing strings
+                * The position of your columns for testing (range_of_columns) and index of the colum for test (index_test). 
+                    It should be a valid entry for pandas' function iloc
+            For the encoding to take place.
 
-                            ---What it return---
-        This function
+                            ---What it returns---
+        This function returns X_train, X_test, y_train, y_test. In that order 
         """
+        if (train_list != None) and (name_test != None):
+            self.X = self.df[train_list]
+            self.y = self.df[test]
+
+        elif (range_of_columns != None) and (index_test != None):
+            self.X = self.df.iloc[:, range_of_columns]
+            self.y = self.df[index_test]
         
-        self.X = self.df[train_list]
-        self.y = self.df[test]
+        else:
+            print('Your input does not compute. Make up your mind!')
+            
 
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y,
                                                                                 test_size= test_size,
-                                                                                random_state= random_state)
-
-
-        if scale_data == True:
-                self.scaler = MinMaxScaler()
-
-                self.X_train = self.scaler.fit_transform(self.X_train)
-                self.X_test = self.scaler.transform(self.X_test)
-
+                                                                                random_state= random_state
+                                                                                shuffle= shuffle,
+                                                                                stratify= stratify)
 
         return self.X_train, self.X_test, self.y_train, self.y_test
-    
+
+
+   def scale_data(self, how=None, name_list= None, 
+                index_range= None, copy=True, with_mean=True, 
+                with_std=True, feature_range=(0, 1), clip=False):
+        """
+        TODO
+                            ---What it does---
+        This function creates a random forest classifier model. Prints it and returns it.
+
+                            ---What it needs---
+            - The sklearn standard arguments necessary
+
+                            ---What it return---
+        This function returns the created model
+        """    
+        standard = ['standard', 'Standard', 'STANDARD']
+        minmax = ['minmax','MinMax', 'MINMAX']
+
+        print(f'You chose a {how} scaler for your data')
+
+        if name_list != None:
+            data = self.df[data]
+        
+        elif index_range != None:
+            data = self.df.iloc[: , index_range]
+
+        if how in standard:
+            scaler = StandardScaler()
+
+        elif how in minmax:
+            scaler = MinMaxScaler()
+
+        else:
+            print(f'{how} is not supported or does not exist.')
+
+        scaled_data = scaler.fit_transform(data)
+
+        return scaled_data
+
 
     def create_linear_or_logistic_model(self, kind= 'linear', fit_intercept=True, 
                             normalize=False, copy_X=True, 
@@ -108,7 +159,7 @@ class Model_Maker():
         This function creates a simple linear/logistic regression model. Prints it and returns it.
 
                             ---What it needs---
-            - The kind of model you wish to create (linear or logistic). Must be string
+            - The kind of model you wish to create (linear or logistic). Must be string. set to 'linear' by default.
             - The sklearn standard arguments necessary
 
                             ---What it return---
